c0fd527150af7a0d5f60cbcc33fa896064209817
gauravssah <124705806+gauravssah@users.noreply.github.com>
2023-12-18 21:41:54 +0530

    solve arry

--- V-09/01HW.cpp
+++ V-09/01HW.cpp
@@ -4,24 +4,36 @@ using namespace std;
 int main()
 {
 
-    int arraySize = 10;
+    // int arraySize = 10;
 
-    int *myArray = new int[arraySize];
+    // int *myArray = new int[arraySize];
 
-    // int siz = sizeof(myArray) / sizeof(int);
+    // // int siz = sizeof(myArray) / sizeof(int);
 
-    cout << "Before : " << arraySize << endl;
+    // cout << "Before : " << arraySize << endl;
 
-    // Get value in array.
-    for (int i = 0; i < arraySize; i++)
+    // // Get value in array.
+    // for (int i = 0; i < arraySize; i++)
+    // {
+    //     cin >> myArray[i];
+    // }
+    // cout << "After : " << arraySize << endl;
+
+    // // Display value in array.
+    // for (int i = 0; i < arraySize; i++)
+    // {
+    //     cout << myArray[i] << endl;
+    // }
+    int arrsize = 10;
+    int arr[arrsize];
+
+    for (int i = 0; i < arrsize; i++)
     {
-        cin >> myArray[i];
+        arr[i] = 21;
     }
-    cout << "After : " << arraySize << endl;
 
-    // Display value in array.
-    for (int i = 0; i < arraySize; i++)
+    for (int i = 0; i < arrsize; i++)
     {
-        cout << myArray[i] << endl;
+        cout << i << ". " << arr[i] << endl;
     }
 }
